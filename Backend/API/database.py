a6e46e0ab883f63873514641769916a5c3182d79
Minh-Tuan-Co <tuanco2003@gmail.com>
2025-02-13 08:18:04 +0100

    added backend functionality and database for mc-tools

---
+++ Backend/API/database.py
@@ -0,0 +1,15 @@
+import sqlite3
+from sqlite3 import Connection
+from fastapi import HTTPException
+
+DATABASE_PATH = '..\\DB\\MultipleChoiceTool.db'
+
+# Database setup
+def get_db_connection() -> Connection:
+    """Returns a SQLite connection."""
+    try:
+        conn = sqlite3.connect(DATABASE_PATH)
+        conn.row_factory = sqlite3.Row  # Allows column access by name
+        return conn
+    except sqlite3.OperationalError as e:
+        raise HTTPException(status_code=500, detail=f"Database connection error: {e}")
